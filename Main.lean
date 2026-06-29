ed737e17f15b20bab248040c8dafa45697320ab6
Yuki Imamura <40051885+imamuray@users.noreply.github.com>
2026-06-29 23:43:31 +0000

    evalを0除算除算に対応

--- Main.lean
+++ Main.lean
@@ -10,19 +10,43 @@ def expr : Expr :=
 #check Nat
 #eval expr
 
-def eval : Expr -> Int
-  | .int n => n
-  | .add a b => eval a + eval b
-  | .sub a b => eval a - eval b
-  | .mul a b => eval a * eval b
-  | .div a b => eval a / eval b
+def eval : Expr -> Except String Int
+  | .int n => .ok n
+  | .add a b =>
+    match eval a, eval b with
+    | .ok x, .ok y => .ok (x + y)
+    | .error e, _ => .error e
+    | _, .error e => .error e
+  | .sub a b =>
+    match eval a, eval b with
+    | .ok x, .ok y => .ok (x - y)
+    | .error e, _ => .error e
+    | _, .error e => .error e
+  | .mul a b =>
+    match eval a, eval b with
+    | .ok x, .ok y => .ok (x * y)
+    | .error e, _ => .error e
+    | _, .error e => .error e
+  | .div a b =>
+    match eval a, eval b with
+      | .ok _, .ok 0 => .error "division by zero"
+      | .ok x, .ok y => .ok (x / y)
+      | .error e, _ => .error e
+      | _, .error e => .error e
 
 def testEval : Expr :=
   .div
     (.sub (.mul (.int 10) (.int 5)) (.int 8))
     (.int 2)
 
+def okExpr : Expr :=
+  .div (.int 10) (.int 2)
+def errExpr : Expr :=
+  .div (.int 10) (.int 0)
+
 #eval eval testEval
+#eval eval okExpr
+#eval eval errExpr
 
 def main : IO Unit :=
   IO.println s!"Hello!"
