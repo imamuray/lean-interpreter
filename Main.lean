45321086faaf328f6fdaae70cfed3573975ac487
Yuki Imamura <40051885+imamuray@users.noreply.github.com>
2026-06-29 23:27:06 +0000

    eval作成

--- Main.lean
+++ Main.lean
@@ -10,5 +10,19 @@ def expr : Expr :=
 #check Nat
 #eval expr
 
+def eval : Expr -> Int
+  | .int n => n
+  | .add a b => eval a + eval b
+  | .sub a b => eval a - eval b
+  | .mul a b => eval a * eval b
+  | .div a b => eval a / eval b
+
+def testEval : Expr :=
+  .div
+    (.sub (.mul (.int 10) (.int 5)) (.int 8))
+    (.int 2)
+
+#eval eval testEval
+
 def main : IO Unit :=
   IO.println s!"Hello!"
