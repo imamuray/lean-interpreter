051269a4cb2131c12868759555c6a24c546c69cc
Yuki Imamura <40051885+imamuray@users.noreply.github.com>
2026-06-30 05:56:54 +0000

    Parser作成

---
+++ Interpreter/Parser.lean
@@ -0,0 +1,57 @@
+import Interpreter.Expr
+
+inductive Token where
+  | int : Int → Token
+  | ident : String → Token
+  | plus
+  | minus
+  | star
+  | slash
+  | lparen
+  | rparen
+deriving Repr
+
+def lexInt (s : String) : Option Token := do
+  let n ← s.toInt?
+  return .int n
+
+def parseAtom : List Token → Option (Expr × List Token)
+  | .int n :: rest =>
+    some (.int n, rest)
+  | .ident x :: rest =>
+    some (.var x, rest)
+  | _ =>
+    none
+
+#eval parseAtom [.int 1, .plus, .int 2]
+#eval parseAtom [.ident "x", .plus, .int 1]
+
+def parseMul : List Token → Option (Expr × List Token)
+  | tokens => do
+    let (left, rest) ← parseAtom tokens
+    match rest with
+    | .star :: rest' =>
+      let (right, rest'') ← parseAtom rest'
+      return (.mul left right, rest'')
+    | .slash :: rest' =>
+      let (right, rest'') ← parseAtom rest'
+      return (.div left right, rest'')
+    | _ =>
+      return (left, rest)
+
+#eval parseMul [.int 2, .star, .ident "x"]
+
+def parseExpr : List Token → Option (Expr × List Token)
+  | tokens => do
+    let (left, rest) ← parseMul tokens
+    match rest with
+    | .plus :: rest' =>
+      let (right, rest'') ← parseMul rest'
+      return (.add left right, rest'')
+    | .minus :: rest' =>
+      let (right, rest'') ← parseMul rest'
+      return (.sub left right, rest'')
+    | _ =>
+      return (left, rest)
+
+#eval parseExpr [.int 1, .plus, .int 2]
